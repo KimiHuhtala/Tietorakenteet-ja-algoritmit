306da9b0b6bf743f011029061eb9d30a04998a59
Kimi <kimihuhtala@gmail.com>
2021-12-17 14:43:48 +0200

    stack

---
+++ Stackk/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+class Main {
+    public static void main(String[] args) {
+        SortAlgorithms.selectSort();
+        SortAlgorithms.resetCounter();
+
+        SortAlgorithms.mergeSort();
+        SortAlgorithms.resetCounter();
+
+        SortAlgorithms.quickSortLoop();
+        SortAlgorithms.printResults();
+    }
+}
